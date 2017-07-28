f1a526559a830e3029d1b696a447864f17849b8e
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-07-28 12:13:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import telebot
 from telebot import types
-
-TOKEN = '419956455:AAGvt0ESW5g7WmHTT9av557_Tm13rjmXCnM'
+ 
+TOKEN = '403760552:AAH6R0EAKTYAvjsOX1noscBStd7XG1WSnw0'
 bot = telebot.TeleBot(TOKEN)
-
-
+ 
 @bot.message_handler(commands=['start'])
 def start(m):
     """Отвечаем на команду /start
@@ -16,7 +15,7 @@ def start(m):
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СОХРАНИТЬ МОДНЫЙ СТИКЕРПАК']])
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СТАТЬ АМБАССАДОРОМ']])
     bot.send_message(m.chat.id, "Привет, я чат-бот MATRIX, который будет сегодня жужжать пчелой для всех гостей ПИКНИКА АФИШИ! Давай расскажу, что мы приготовили для тебя сегодня:", reply_markup=keyboard)
-
+ 
 @bot.callback_query_handler(func= lambda c: True)
 def inline(c):
     if c.data == 'СДЕЛАТЬ КРУТЫЕ БРЭЙДЫ':
@@ -28,6 +27,16 @@ def inline(c):
     elif c.data == 'СОХРАНИТЬ МОДНЫЙ СТИКЕРПАК':
         bot.send_message(chat_id=c.message.chat.id, text="Лови набор стикеров!")
     elif c.data == 'СТАТЬ АМБАССАДОРОМ':
-        bot.send_message(chat_id=c.message.chat.id, text="Этой осенью запустится профессиональная эко-гамма по уходу за волосами Biolage RAW. Это не просто продукты, это манифест иного образа жизни. Хочешь узнать первым и стать лидером мнений новой гаммы? Оставляй свой e-mail, и мы отправим тебе инструкцию. #liveRAW")
-
+        bot.send_message(chat_id=c.message.chat.id, text='Этой осенью запустится профессиональная эко-гамма по уходу за волосами Biolage RAW. Это не просто продукты, это манифест иного образа жизни. Хочешь узнать первым и стать лидером мнений новой гаммы? Оставляй свой e-mail, и мы отправим тебе инструкцию. /ttttt')
+        
+            
+@bot.message_handler(commands=['ttttt'])
+def ttttt(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+       
+def hello(message):
+    bot.send_message(message.chat.id, 'Привет, {name}. dsafdsafadsf.'.format(name=message.text))
+       
+ 
 bot.polling()
